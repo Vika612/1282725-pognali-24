7962ba82a3d74c54b73ce6c27263a546504c1846
Victoria Domovets <v.domovets@gmail.com>
2022-01-02 14:43:28 +0100

    блок фильтра на странице каталога

--- source/js/main.js
+++ source/js/main.js
@@ -1,43 +1,5 @@
 'use strict';
 
-/* MENU */
-
-const headerBody = document.querySelector('.header');
-const menuToggle = document.querySelector('.menu-toggle');
-const navigation = document.querySelector('.navigation');
-
-headerBody.classList.add('header--js');
-navigation.classList.add('header__navigation--closed');
-
-if (menuToggle) {
-  menuToggle.addEventListener('click', function () {
-    menuToggle.classList.toggle('active');
-    navigation.classList.toggle('header__navigation--closed');
-    headerBody.classList.toggle('header--opened');
-  });
-}
-
-/* TABS */
-
-const tabNavs = document.querySelectorAll('.alphabet-filter__link');
-
-tabNavs.forEach((tabNav) => {
-
-  tabNav.addEventListener('click', (e) => {
-    e.preventDefault();
-    const activeTabAttr = e.target.dataset.tab;
-
-    document.querySelector('.alphabet-filter__link.active').classList.remove('active');
-    document.querySelector('.countries-filter__item.active').classList.remove('active');
-
-    tabNav.classList.add('active');
-
-    const activeScreen = `.countries-filter__item--${activeTabAttr}`;
-
-    document.querySelector(activeScreen).classList.add('active');
-  });
-});
-
 /* SCROLL */
 
 const header = document.querySelector('.header');
@@ -51,12 +13,55 @@ const header = document.querySelector('.header');
   };
 
 
+/* MENU */
+
+const headerBody = document.querySelector('.header');
+const menuToggle = document.querySelector('.menu-toggle');
+const navigation = document.querySelector('.navigation');
+
+headerBody.classList.add('header--js');
+navigation.classList.add('header__navigation--closed');
+
+if (menuToggle) {
+  menuToggle.addEventListener('click', () => {
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('header__navigation--closed');
+    headerBody.classList.toggle('header--opened');
+  });
+}
+
+
+/* TABS */
+
+const tabNavs = document.querySelectorAll('.alphabet-filter__link');
+
+if (tabNavs) {
+  tabNavs.forEach((tabNav) => {
+
+    tabNav.addEventListener('click', (e) => {
+      e.preventDefault();
+      const activeTabAttr = e.target.dataset.tab;
+
+      document.querySelector('.alphabet-filter__link.active').classList.remove('active');
+      document.querySelector('.countries-filter__item.active').classList.remove('active');
+
+      tabNav.classList.add('active');
+
+      const activeScreen = `.countries-filter__item--${activeTabAttr}`;
+
+      document.querySelector(activeScreen).classList.add('active');
+    });
+  });
+}
+
+
   /* MODAL */
 
 const openBtn = document.querySelector('.add-profile__link');
 const closeBtn = document.querySelector('.modal__close');
 const popup = document.querySelector('.modal');
 
+if (openBtn) {
   openBtn.addEventListener('click', (e) => {
     e.preventDefault();
     popup.classList.add('modal--show');
@@ -72,3 +77,24 @@ const popup = document.querySelector('.modal');
       popup.classList.remove('modal--show');
     }
   });
+}
+
+
+/* FILTER */
+
+const filterContainer = document.querySelector('.filter__container');
+const filterToggle = document.querySelector('.filter-toggle');
+const filterClose = document.querySelector('.dropdown-filter__close');
+
+filterContainer.classList.add('filter__container--closed');
+filterContainer.classList.add('filter__container--js');
+
+filterToggle.addEventListener('click', () => {
+  filterContainer.classList.toggle('filter__container--closed');
+  filterToggle.classList.toggle('active');
+});
+
+filterClose.addEventListener('click', () => {
+  filterContainer.classList.add('filter__container--closed');
+  filterToggle.classList.toggle('active');
+});
