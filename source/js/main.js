a098f4f47dc6fe09fa65f45b1e1d7046d1ff1f50
Victoria Domovets <v.domovets@gmail.com>
2022-01-17 19:12:26 +0100

    блок calendar-slider: стилизация, адаптив и функционал

--- source/js/main.js
+++ source/js/main.js
@@ -148,3 +148,48 @@ filterClose.addEventListener('click', () => {
   filterToggle.classList.toggle('active');
 });
 }
+
+/* SLIDER */
+
+let position = 0;
+const slidesToShow = 1;
+const slidesToScroll = 1;
+const container = document.querySelector('.calendar-slider');
+const track = document.querySelector('.calendar-slider__list');
+const btnPrev = document.querySelector('.calendar-slider__button--prev');
+const btnNext = document.querySelector('.calendar-slider__button--next');
+const items = document.querySelectorAll('.calendar-slider__item');
+const itemsCount = items.length;
+const itemWidth = container.clientWidth / slidesToShow;
+const movePosition = slidesToScroll * itemWidth;
+
+items.forEach((item) => {
+  item.style.minWidth = `${itemWidth}px`;
+});
+
+btnNext.addEventListener('click', () => {
+  const itemsLeft = itemsCount - (Math.abs(position) + slidesToShow * itemWidth) / itemWidth;
+
+  position -= itemsLeft >= slidesToScroll ? movePosition : itemsLeft *itemWidth;
+  setPosition();
+  checkBtns();
+});
+
+btnPrev.addEventListener('click', () => {
+  const itemsLeft = Math.abs(position) / itemWidth;
+
+  position += itemsLeft >= slidesToScroll ? movePosition : itemsLeft *itemWidth;
+  setPosition();
+  checkBtns();
+});
+
+const setPosition = () => {
+  track.style.transform = `translateX(${position}px)`;
+};
+
+const checkBtns = () => {
+  btnPrev.disabled = position === 0;
+  btnNext.disabled = position <= -(itemsCount - slidesToShow) * itemWidth;
+};
+
+checkBtns();
